a93e944403dfd760e3252949d8b5a5eed901da14
Alfiya Paukova <alfiya.paukova92@gmail.com>
2022-09-04 17:14:56 +0300

    add Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int miles = 20;
+        int price = 1000;
+
+        System.out.println((price / miles) + " начисленных милей");
+    }
 }
